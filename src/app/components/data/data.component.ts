5939694f6c3c0f55f24d87d69aff7918da0d2822
leonardo15043 <lahernandez15043@gmail.com>
2019-01-21 17:53:19 -0500

    Aproximación por data : Agrupacion de campos del formulario en nuestro objeto con (formGroupName)

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -13,9 +13,13 @@ export class DataComponent  {
   constructor() {
 
     this.forma = new FormGroup({
-      'nombre': new FormControl('',  [ Validators.required , Validators.minLength(4) ] ),
-      'apellido': new FormControl('', Validators.required),
+
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('',  [ Validators.required , Validators.minLength(4) ] ),
+        'apellido': new FormControl('', Validators.required),
+      }),
       'correo': new FormControl('', [ Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$") ] ),
+
     })
 
   }
