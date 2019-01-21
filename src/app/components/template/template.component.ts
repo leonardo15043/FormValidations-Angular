c8e84704461fa19f63b030549f3f6d1f51899b31
leonardo15043 <lahernandez15043@gmail.com>
2019-01-21 09:12:59 -0500

    Aproximación por template : Obtener datos y cargar datos por defecto

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent  {
+
+  usuario:Object = {
+    nombre: "Arturo",
+    apellido: "Espinosa",
+    email: "prueba@gmail.com"
+  }
 
   constructor() { }
 
-  ngOnInit() {
+  guardar( forma:NgForm ){
+    console.log( this.usuario );
   }
 
 }
